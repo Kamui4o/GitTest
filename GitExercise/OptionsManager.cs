53bc3aeedfae5de281c30763024d8adca9afa758
Anreshko <Topa>
2024-03-17 11:24:39 +0200

    Log, Power, Fact options added

--- GitExercise/OptionsManager.cs
+++ GitExercise/OptionsManager.cs
@@ -9,7 +9,12 @@ namespace GitExercise
             "s - Subtract",
             "m - Multiply",
             "d - Divide",
-            "sabs - Subtract ABS"
+            "sabs - Subtract ABS",
+            "dr - Divide Remainder",
+            "ex - Exit",
+            "pow - Power",
+            "log - Logarithm",
+            "fact - Sum of 2 factorials"
         };
 
         public static void Add(double a, double b)
@@ -34,6 +39,35 @@ namespace GitExercise
         public static void SubtractABS(double a, double b)
         {
             Console.WriteLine($"{a} - {b} = {Math.Abs(a - b)}");
+        }
+
+        public static void DivideRemainder(double a , double b)
+        {
+            Console.WriteLine($"{a} % {b} = {a % b}");
+        }
+
+        private static long CalculateFactorial(int a)
+        {
+            long result = a;
+            for (int i = a - 1; i >= 1 ; i--)
+            {
+                result *= i;
+            }
+            return result;
+        }
+        public static void Factorial(int a, int b)
+        {
+            Console.WriteLine($"{a}! + {b}! = {CalculateFactorial(a) + CalculateFactorial(b)}");
+        }
+
+        public static void Power(double a, double b)
+        {
+            Console.WriteLine($"{a} ^ {b} = {Math.Pow(a, b)}");
+        }
+
+        public static void Log(double a, double b)
+        {
+            Console.WriteLine($"Log of {a} with base {b} = {Math.Log(a, b)}");
         }
     }
 }
--- GitExercise/Startup.cs
+++ GitExercise/Startup.cs
@@ -18,46 +18,64 @@ namespace GitExercise
                 Console.ReadKey(true);
                 return;
             }
-            Console.WriteLine("Console Calculator App");
-            Console.WriteLine(new string('-', 15));
-
-            Console.Write("a = ");
-            double a = double.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
-
-            Console.Write("b = ");
-            double b = double.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
-
-            Console.WriteLine("Choose one from the listed options:");
-            foreach (string option in OptionsManager.OptionsList)
+            while (true)
             {
-                Console.WriteLine($"\t{option}");
+                Console.Clear();
+
+                Console.WriteLine("Console Calculator App");
+                Console.WriteLine(new string('-', 15));
+
+                Console.Write("a = ");
+                double a = double.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+
+                Console.Write("b = ");
+                double b = double.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+
+                Console.WriteLine("Choose one from the listed options:");
+                foreach (string option in OptionsManager.OptionsList)
+                {
+                    Console.WriteLine($"\t{option}");
+                }
+
+                Console.Write("Option: ");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "a":
+                        OptionsManager.Add(a, b);
+                        break;
+                    case "s":
+                        OptionsManager.Subtract(a, b);
+                        break;
+                    case "m":
+                        OptionsManager.Multiply(a, b);
+                        break;
+                    case "d":
+                        OptionsManager.Divide(a, b);
+                        break;
+                    case "sabs":
+                        OptionsManager.SubtractABS(a, b);
+                        break;
+                    case "exit":
+                        Console.Clear();
+                        Console.WriteLine("Goodbye!");
+                        Console.ReadKey(true);
+                        return;
+                    case "pow":
+                        OptionsManager.Power(a, b);
+                        break;
+                    case "log":
+                        OptionsManager.Log(a, b);
+                        break;
+                    case "fact":
+                        OptionsManager.Factorial((int)a, (int)b);
+                        break;
+
+                }
+                Console.WriteLine("Pres any key to continue...");
+                Console.ReadKey(true);
             }
-
-            Console.Write("Option: ");
-            string choice = Console.ReadLine();
-
-            switch (choice)
-            {
-                case "a":
-                    OptionsManager.Add(a, b);
-                    break;
-                case "s":
-                    OptionsManager.Subtract(a, b);
-                    break;
-                case "m":
-                    OptionsManager.Multiply(a, b);
-                    break;
-                case "d":
-                    OptionsManager.Divide(a, b);
-                    break;
-                case "sabs":
-                    OptionsManager.SubtractABS(a, b);
-                    break;
-
-            }
-
-            Console.WriteLine("Pres any key to close the app...");
-            Console.ReadKey(true);
         }
     }
 }
